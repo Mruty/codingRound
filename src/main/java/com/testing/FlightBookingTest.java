8a188ed945d689a5282623f21b36e5565acf2e39
Mrutyunjay swain <mrutyunjay.aei@gmail.com>
2017-11-30 12:55:20 -0500

    FlightBookingTest script modified There was problem  with finding elements.It is resolved

--- src/main/java/com/testing/FlightBookingTest.java
+++ src/main/java/com/testing/FlightBookingTest.java
@@ -26,8 +26,8 @@ public class FlightBookingTest extends BaseTest{
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
         originOptions.get(0).click();
 
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
+        driver.findElement(By.id("ToTag")).clear();
+        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
 
         //wait for the auto complete options to appear for the destination
 
@@ -35,8 +35,8 @@ public class FlightBookingTest extends BaseTest{
         //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
-
-        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
+  
+        driver.findElement(By.xpath(".//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[5]/td[3]/a")).click();
 
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
--- src/main/java/com/testing/SignInTest.java
+++ src/main/java/com/testing/SignInTest.java
@@ -1,8 +1,6 @@
 package com.testing;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -21,8 +19,8 @@ public class SignInTest extends BaseTest{
         driver.findElement(By.id("SignIn")).click();
         wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("modal_window"));
         
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("signInButton")));
-        driver.findElement(By.id("signInButton")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("signInButton"))).click();
+       
 
         String errors1 = driver.findElement(By.id("errors1")).getText();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
